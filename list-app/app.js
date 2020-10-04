cc30cd17a07e08467396d0a01727e27b4fd75eb0
ihsanuddienullah <ihsanuddienullah@gmail.com>
2020-10-04 21:20:27 +0700

    sql

--- list-app/app.js
+++ list-app/app.js
@@ -1,6 +1,32 @@
 const express = require('express');
+const mysql = require('mysql');
 
 const app = express();
+
+const conn = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'progate_list_app'
+});
+
+conn.connect((err) => {
+    if (err) {
+        console.log('error connecting: ' + err.stack);
+        return;
+    }
+    console.log('Success');
+})
+
+app.get('/', (req, res) => {
+    conn.query(
+      'SELECT * FROM users',
+        (error, results) => {
+        console.log(results);
+        res.render('hello.ejs');
+        }
+    );
+});
 
 app.get('/', (req, res) => {
     res.render('hello.ejs');
